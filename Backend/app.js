97c513f5689b3e27bf5716a8a93ce0478003bf7c
Eric Dickson <edickson113@rku.ac.in>
2025-09-03 06:35:28 +0530

    worked on product model and controller

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,26 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler, notFound } from "./Middleware/error.middleware.js";
+import authRoute from "./routes/auth.route.js";
+import productRoute from "./routes/product.route.js";
 const app = express();
+
+// CORS
+app.use(cors());
 
 // JSON BODY
 app.use(express.json());
 
-// CORS
-app.use(cors());
+// COOKIE PARSER
+app.use(cookieParser());
+
+// Routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/products", productRoute);
+
+// Error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
